dccc3a516c58db86bc4b5cab254773d2e6ff2373
Igor Yankovich <irsy0304@gmail.com>
2023-10-07 17:15:51 -0400

    HW4_fix_python_variable_string

--- test_string.py
+++ test_string.py
@@ -3,10 +3,10 @@ import python_string
 
 class TestProgram(unittest.TestCase):
     def test_new_string(self):
-        a = "Hello"
-        b = "It"
-        c = "-Academy"
-        expected_result = "Hello It-Academy!"
+        a = "a"
+        b = "b"
+        c = "c"
+        expected_result = "abc"
         new_string = python_string.concatenate_strings(a, b, c)
         self.assertEqual(new_string, expected_result)
 
